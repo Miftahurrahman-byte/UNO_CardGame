10091d725d7ecd63a0b59b06b5bfca947b1e558a
akudafa28-dot <akudafa28@gmail.com>
2026-06-22 14:50:43 +0800

    baru oyy

--- bagian_dafa.cpp
+++ bagian_dafa.cpp
@@ -1,17 +1,49 @@
-while (sisapemain > 1)
-    {
-        cout << "\033[2J\033[1;1H";
+else if (kartudipilih.warna == "Wild")
+                {
+                    int warnaBaru = 0;
+                    while (warnaBaru < 1 || warnaBaru > 4 || cin.fail())
+                    {
+                        cin.clear();
+                        cin.ignore(10000, '\n');
+                        cout << "\nPilih warna baru:\n1. Red\n2. Yellow\n3. Green\n4. Blue\nPilihan (1-4): ";
+                        cin >> warnaBaru;
+                    }
 
-        cout << "=========================================\n";
-        cout << "STATUS MEJA SAAT INI:\n";
-        cetakKartuMeja(warnasekarang, angkasekarang);
-        cout << "Warna Main   : \033[1m" << getWarnaKode(warnasekarang) << warnasekarang << "\033[0m\n";
-        cout << "Arah Putaran : " << (arahmain ? "Searah Jarum Jam ->" : "<- Berlawanan Jarum Jam") << "\n";
-        cout << "Sisa pemain aktif: " << sisapemain << "\n";
-        cout << "=========================================\n";
-        cout << "\nGILIRAN SEKARANG: \033[1;32m" << daftarpemain[giliran].namapemain << "\033[0m\n\n";
+                    if (warnaBaru == 1)
+                        warnasekarang = "Red";
+                    else if (warnaBaru == 2)
+                        warnasekarang = "Yellow";
+                    else if (warnaBaru == 3)
+                        warnasekarang = "Green";
+                    else
+                        warnasekarang = "Blue";
 
-        ditangan(&daftarpemain[giliran]);
+                    if (kartudipilih.angka == "Wild Draw4")
+                    {
+                        int target = giliran;
+                        do
+                        {
+                            if (arahmain)
+                                target = (target + 1) % totalpemain;
+                            else
+                                target = (target - 1 + totalpemain) % totalpemain;
+                        } while (!daftarpemain[target].aktif);
+
+                        for (int k = 0; k < 4; k++)
+                        {
+                            daftarpemain[target].kartuditangan[daftarpemain[target].sisakartu] = ambilkartu();
+                            daftarpemain[target].sisakartu++;
+                        }
+                        cout << "\n"
+                             << daftarpemain[target].namapemain << " Kena +4 dan gilirannya dilewati!\n";
+                        giliran = target;
+                        cout << "Tekan Enter untuk melanjutkan...";
+                        cin.ignore();
+                        cin.get();
+                    }
+                }
+            }
+        }
 
         if (daftarpemain[giliran].sisakartu == 1)
         {
